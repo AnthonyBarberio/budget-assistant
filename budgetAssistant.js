231abf0c1e71a967a59f5d619d022bfe705da684
AnthonyBarberio <barberioa@usf.edu>
2025-06-01 15:58:53 -0400

    Update budgetAssistant.js

--- budgetAssistant.js
+++ budgetAssistant.js
@@ -1,1 +1,15 @@
+// Budgeting Assistant
 
+let startupName = "BrightStart Tech";
+let initialCapital = 50000;
+let monthlyRevenue = 20000;
+let monthlyExpenses = 15000;
+
+let netCashFlow = monthlyRevenue - monthlyExpenses;
+let currentBalance = initialCapital + netCashFlow;
+let isProfitable = netCashFlow > 0;
+
+console.log("Startup Name: " + startupName);
+console.log("Net Monthly Cash Flow: $" + netCashFlow);
+console.log("Current Balance: $" + currentBalance);
+console.log("Profitability Status: " + isProfitable);
